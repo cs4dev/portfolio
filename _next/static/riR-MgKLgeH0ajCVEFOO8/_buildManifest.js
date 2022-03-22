9d779d12e802c9b5c8fe4de3f13648debb9571b4
cs4dev <101970677+cs4dev@users.noreply.github.com>
2022-03-22 13:11:11 +0000

    Deploying to gh-pages from @ cs4dev/portfolio@14addfaf031125af65682a8c8eda21aacbb0d9d5 🚀

---
+++ _next/static/riR-MgKLgeH0ajCVEFOO8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/0b099c53820f1340.css","static/chunks/pages/index-ccb115030229a5f3.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
