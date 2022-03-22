58b4d3dd5954e7806511c69967c8e2735907aeed
cs4dev <cs4dev@gmail.com>
2022-03-22 21:32:15 +0800

    fix favicon path

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,17 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { prefix } from '../prefix'
 
-const Home: NextPage = () => {
+const faviconPath = prefix ? `${prefix}/favicon.ico?v=1` : '/favicon.ico?v=1';
+
+const Home: NextPage = () => {  
   return (
     <div className={styles.container}>
       <Head>
         <title>4dev</title>
         <meta name="description" content="cs4dev portfolio" />
-        <link rel="icon" href="/favicon.ico?v=2" />
+        <link rel="icon" href={faviconPath} />
       </Head>
 
       <main className={styles.main}>
